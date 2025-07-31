a187dca108ad80a7a1a15378f0c93c007e299a02
demirel <35367173+harundemirel@users.noreply.github.com>
2025-07-31 23:00:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 APP = ['FolderCreatorGUI_Updated_UI.py']
+DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'iconfile': 'icon.icns',  # Bu dosyayı bir sonraki adımda ekleyeceğiz
-    'packages': ['tkinter'],
+    'iconfile': 'icon.icns',
+    'packages': [],
 }
 
 setup(
     app=APP,
+    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
